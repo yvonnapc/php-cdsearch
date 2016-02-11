c9d8d99ceaf7782c3617c4f4a0e325f5419dbffd
Michael Lor and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-11 11:50:48 -0800

    Add CD class, silex, and twig

--- app/app.php
+++ app/app.php
@@ -2,6 +2,15 @@
       require_once __DIR__."/../vendor/autoload.php";
       require_once __DIR__."/../src/cd.php";
 
-      $app = new Silex\Application();
+      session_start();
+      if (empty($_SESSION['list_of_cds'])){
+        $_SESSION['list_of_cds'] = array();
+      }
 
+      $app = new Silex\Application();
+      $app->register(new Silex\Provider\TwigServiceProvider, array(
+        'twig.path' => __DIR__."/../views"
+      ));
+
+      
  ?>
--- src/cd.php
+++ src/cd.php
@@ -1,3 +1,44 @@
 <?php
-class CD
- ?>
+
+  class CD
+  {
+    private $artist;
+    private $album;
+
+  function _construct($cd_artist, $cd_album)
+  {
+    $this->artist = $artist;
+    $this->album = $album;
+  }
+  function setArtist($new_artist)
+  {
+    $this->artist = $new_artist;
+  }
+  function setAlbum($new_album)
+  {
+    $this->album = $new_album;
+  }
+  function getArtist()
+  {
+    return $this->artist;
+  }
+  function getAlbum()
+  {
+    return $this->album;
+  }
+  function save()
+  {
+    array_push($_SESSION['list_of_cds'], $this);
+  }
+  static function getAll()
+  {
+  return $_SESSION['list_of_cds'];
+  }
+  static function deleteAll()
+  {
+    $_SESSION['list_of_cds'] = array();
+  }
+  
+}
+
+?>
